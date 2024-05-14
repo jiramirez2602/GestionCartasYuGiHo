7a0ac902d2393e319cf50b00c7dbaf14433319a5
Tzaerk <149003552+PepeTza@users.noreply.github.com>
2024-05-14 03:23:00 +0000

    Falta la funcionalidad de los botones y la conexion con la db

--- my-app/src/routes/records/+page.svelte
+++ my-app/src/routes/records/+page.svelte
@@ -1,5 +1,8 @@
 <script>
+
   let menu= "Records"
+  let formato = ["06-2001"]
+  let pagina = 0;
 </script>
 
 <body id="page-top">
@@ -158,7 +161,68 @@
           <div
             class="d-sm-flex align-items-center justify-content-between mb-4"
           >
-            <h1 class="h3 mb-0 text-gray-800">{menu}</h1>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
+          <div class="container mt-3 mb-4">
+          <div class="col-lg-9 mt-4 mt-lg-0">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="user-dashboard-info-box table-responsive mb-0 bg-white p-4 shadow-sm">
+                    {#if formato[0]!=null && pagina == 0}
+                    <table class="table manage-candidates-top mb-0">
+                      <thead>
+                        <tr>
+                          <th>Formato</th>
+                          <th class="text-center">Estado</th>
+                          <th class="action text-right">Acciones</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {#each formato as e, num}
+                        {#if num+1 <= 6}
+                        <tr class="candidates-list">
+                          <td class="title">
+                            <div class="candidate-list-details">
+                              <div class="candidate-list-info">
+                                <div class="candidate-list-title">
+                                  <span class="candidate-list-time order-1">{e}</span>
+                                </div>
+                              </div>
+                            </div>
+                          </td>
+                          <td class="candidate-list-favourite-time text-center">
+                            {#if num==0}
+                            <a class="candidate-list-favourite order-2 text-danger"><i class="fas fa-heart"></i></a>
+                            <span class="candidate-list-time order-1">Actual</span>
+                            {:else}
+                            <span class="candidate-list-time order-1">Legado</span>
+                            {/if}
+                          </td>
+                          <td>
+                            <ul class="list-unstyled mb-0 d-flex justify-content-end">
+                              <li><a href="#" class="text-primary" data-toggle="tooltip" title="" data-original-title="view"><i class="far fa-eye"></i></a></li>
+                              <li><a href="#" class="text-info" data-toggle="tooltip" title="" data-original-title="Edit"><i class="fas fa-pencil-alt"></i></a></li>
+                              <li><a href="#" class="text-danger" data-toggle="tooltip" title="" data-original-title="Delete"><i class="far fa-trash-alt"></i></a></li>
+                            </ul>
+                          </td>
+                        </tr>
+                        {/if}
+                        {/each}
+                      </tbody>
+                    </table>
+                    <div class="text-center mt-3 mt-sm-3">
+                      <button class="btn btn-primary">Insertar nuevo formato</button>
+                    </div>
+                    {:else if formato[0]==null && pagina == 0}
+                    <div class="text-center mt-3 mt-sm-3">
+                      <button class="btn btn-primary">Insertar nuevo formato</button>
+                      <br><br>
+                    </div>
+                    {/if}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
             
           </div>
         </div>
@@ -176,5 +240,100 @@
 <style>
   .sidebar {
     background-color: rgb(242, 91, 44);
+  }
+  .p-4 {
+    padding: 1.5rem!important;
+  }
+  .mb-0, .my-0 {
+      margin-bottom: 0!important;
+  }
+  .shadow-sm {
+      box-shadow: 0 .125rem .25rem rgba(0,0,0,.075)!important;
+  }    
+
+  .user-dashboard-info-box .title {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+      padding: 30px 0;
+  }
+
+  .user-dashboard-info-box .candidates-list td {
+      vertical-align: middle;
+  }
+
+  .user-dashboard-info-box td li {
+      margin: 0 4px;
+  }
+
+  .user-dashboard-info-box .table thead th {
+      border-bottom: none;
+  }
+
+  .table.manage-candidates-top th {
+      border: 0;
+  }
+
+  .user-dashboard-info-box .candidate-list-favourite-time .candidate-list-favourite {
+      margin-bottom: 10px;
+  }
+
+  .table.manage-candidates-top {
+      min-width: 650px;
+  }
+
+  .user-dashboard-info-box .candidate-list-details ul {
+      color: #969696;
+  }
+
+  /* Candidate List */
+
+  .candidate-list-title {
+      margin-bottom: 5px;
+  }
+
+  .candidate-list-details ul li {
+      margin: 5px 10px 5px 0px;
+      font-size: 13px;
+  }
+
+  .candidate-list .candidate-list-favourite-time span {
+      display: block;
+      margin: 0 auto;
+  }
+  .candidate-list .candidate-list-favourite-time .candidate-list-favourite {
+      display: inline-block;
+      position: relative;
+      height: 40px;
+      width: 40px;
+      line-height: 40px;
+      border: 1px solid #eeeeee;
+      border-radius: 100%;
+      text-align: center;
+      -webkit-transition: all 0.3s ease-in-out;
+      transition: all 0.3s ease-in-out;
+      margin-bottom: 20px;
+      font-size: 16px;
+      color: #646f79;
+  }
+  .candidate-list .candidate-list-favourite-time .candidate-list-favourite:hover {
+      background: #ffffff;
+      color: #e74c3c;
+  }
+
+  .bg-white {
+      background-color: #ffffff !important;
+  }
+  .p-4 {
+      padding: 1.5rem!important;
+  }
+  .mb-0, .my-0 {
+      margin-bottom: 0!important;
+  }
+  .shadow-sm {
+      box-shadow: 0 .125rem .25rem rgba(0,0,0,.075)!important;
   }
 </style>
